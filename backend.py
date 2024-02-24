55e2400ccc415ae6ba8e5531c7057208627a56fc
knight433 <dhruvanu3@gmail.com>
2024-02-24 12:26:18 +0530

    comparePlayer

--- backend.py
+++ backend.py
@@ -15,7 +15,29 @@ class database:
 
         self.db.addPlayer(name,matches,role,team,bowlingType,battingType,battingdic,bowlingdic)
     
-    def compareTeam(self,team1,team2):
-        
+    def compareTeam(self, team1, team2):
         team1ID = self.db.getAllFromTeam(team1)
         team2ID = self.db.getAllFromTeam(team2)
+
+        team1info = {}
+        team2info = {}
+
+        for i in team1ID:
+            tempName = self.db.playerInfo(i, 'name')
+            tempRole = self.db.playerInfo(i, 'role')
+            tempBatting = self.db.playerInfo(i, 'batting_hand')
+            tempBowling = self.db.playerInfo(i, 'bowlingType')
+            tempList = [tempName, tempRole, tempBatting, tempBowling]
+
+            team1info[i] = tempList
+
+        for i in team2ID:
+            tempName = self.db.playerInfo(i, 'name')
+            tempRole = self.db.playerInfo(i, 'role')
+            tempBatting = self.db.playerInfo(i, 'batting_hand')
+            tempBowling = self.db.playerInfo(i, 'bowlingType')
+            tempList = [tempName, tempRole, tempBatting, tempBowling]
+
+            team2info[i] = tempList
+
+        return team1info, team2info
---
+++ main.py
@@ -0,0 +1,54 @@
+# app.py
+from flask import Flask, render_template, request, jsonify,url_for,redirect
+import backend 
+
+app = Flask(__name__)
+con = backend.database()
+
+def testFuntion(player_info,batting_dic,bowling_dic):
+    
+    for key in batting_dic.keys():
+        print(key,type(batting_dic[key]))
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/addPlayer')
+def addPlayersPage():
+    return render_template('addPlayer.html')
+
+@app.route('/comparePlayer')
+def compareTeamPage():
+    return render_template('compareTeams.html')
+
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        
+        data = request.jsons
+
+        player_info = data.get('playerInfo', {})
+        batting_dic = data.get('battingDic', {})
+        bowling_dic = data.get('bowlingDic', {})
+
+        # testFuntion(player_info,batting_dic,bowling_dic)
+
+        con.addPlayer(player_info,batting_dic,bowling_dic)
+        
+
+        return jsonify({'message': 'Data received successfully'}), 200
+
+@app.route('/compareTeam', methods=['POST'])
+def compare_team():
+    if request.method == 'POST':
+        team1 = request.form.get('team1')
+        team2 = request.form.get('team2')
+
+        team1_info, team2_info = con.compareTeam(team1, team2)
+        para = [team1_info,team2_info]
+        return render_template('compareWithPlayers.html',team=para)
+
+if __name__ == '__main__':
+    app.run(debug=True)
